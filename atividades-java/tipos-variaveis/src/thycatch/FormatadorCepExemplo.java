88278aa2ba579ba452bc9ec2778aa73df8d4c19b
Brunagarstka <bruna.luisa.garstkas2@gmail.com>
2024-06-16 23:45:30 -0300

    subindo

---
+++ atividades-java/tipos-variaveis/src/thycatch/FormatadorCepExemplo.java
@@ -0,0 +1,21 @@
+package thycatch;
+
+public class FormatadorCepExemplo{
+   /* ESTA DANDO ERRO POR ISSO EM COMENTARIO
+    public static void main(String[] args) {
+      try {
+        String cepFormatado = formatarCep("23765064"); 
+        System.out.println(cepFormatado);
+      } catch (CepInvalidoException e) {
+        
+      }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+         throw new CepInvalidoException();
+
+        return "23.765-064";
+    }
+        */
+}
+  
